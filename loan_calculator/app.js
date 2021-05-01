6065786431377b5e273399a8f6bbe78341f1af5e
anurag-shukla8004 <79192104+anurag-shukla8004@users.noreply.github.com>
2021-05-01 10:41:57 +0530

    Add files via upload

--- loan_calculator/app.js
+++ loan_calculator/app.js
@@ -1,8 +1,21 @@
 // Listen for submit
-document.getElementById('loan-form').addEventListener('submit', calculateResults);
+document.getElementById('loan-form').addEventListener('submit', function(e){
+
+
+  // Hide results
+
+  document.getElementById('results').style.display = 'none';
+
+  // Show loader
+  document.getElementById('loading').style.display = 'block';
+
+
+  setTimeout(calculateResults, 2000)
+  e.preventDefault();
+});
 
 // Calculate Results
-function calculateResults(e){
+function calculateResults( ){
   console.log('Calculating...');
   // UI Vars
   const amount = document.getElementById('amount');
@@ -24,9 +37,57 @@ function calculateResults(e){
     monthlyPayment.value = monthly.toFixed(2);
     totalPayment.value = (monthly * calculatedPayments).toFixed(2);
     totalInterest.value = ((monthly * calculatedPayments)-principal).toFixed(2);
+
+// Show result
+  document.getElementById('results').style.display = 'block';
+
+  // Hide loader
+
+  document.getElementById('loading').style.display = 'none';
+
+
   } else {
     showError('Please check your numbers');
   }
 
-  e.preventDefault();
+  
+}
+
+// Show error
+function showError(error){
+
+  // Hide results
+
+  document.getElementById('results').style.display = 'none';
+
+  // Show loader
+  document.getElementById('loading').style.display = 'none';
+
+  // Create a  
+const errorDiv = document.createElement('div');
+
+// Get element
+
+const card = document.querySelector('.card');
+const heading = document.querySelector('.heading');
+
+//Add class
+
+errorDiv.className = 'alert alert-danger';
+
+// Create text node and append to div
+
+errorDiv.appendChild(document.createTextNode(error));
+
+// Insert error above heading
+
+card.insertBefore(errorDiv, heading);
+
+// cleat error  after 3 second
+setTimeout(clearError, 3000);
+}
+
+// clear error
+function clearError(){
+  document.querySelector('.alert').remove(); 
 }
